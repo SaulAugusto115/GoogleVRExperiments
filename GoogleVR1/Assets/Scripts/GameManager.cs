977a0c3511c71687be53a47ba1b1499284da6c04
Saul Augusto Gasca Farrera <saulagf115@gmail.com>
2020-11-24 23:56:29 -0600

    UI Added
    Screen Fade and Text Mehs Pro Added

---
+++ GoogleVR1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+
+    public Image imgFade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Fade", 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Fade()
+    {
+        imgFade.CrossFadeAlpha(0, 2, true);
+    }
+}
